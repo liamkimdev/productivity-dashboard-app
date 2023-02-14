6dfad9eecdf6bc712e910c8856b2a665f04b936a
Liam-Kim <liam.kim744@gmail.com>
2023-02-14 12:15:45 -0500

    Initial Commit

---
+++ server/src/test/java/org/productivity/ProductivityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.productivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivityDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
