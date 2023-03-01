cf432642fe35ec2e642ad633ba626c4874c9c9c5
Matt Heine <102334843+mattheine@users.noreply.github.com>
2023-03-01 11:20:41 -0700

    updated production.sql

--- database/production.sql
+++ database/production.sql
@@ -44,6 +44,7 @@ CREATE TABLE note (
 	title VARCHAR(255) default null,
 	description VARCHAR(10000) default null,
 	"timestamp" timestamptz not null,
+    note_widget_id int not null,
     constraint fk_note_note_widget
 		foreign key (note_widget_id)
         references note_widget (note_widget_id)
